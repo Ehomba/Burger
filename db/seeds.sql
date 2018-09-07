ba81a3daebfc59b1f997e2fc049ee8c43ec4504c
Ehomba <Milesbenovic@gmail.com>
2018-09-07 16:45:18 -0500

    almost done

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Regular');
+INSERT INTO burgers (burger_name) VALUES ('Spicy');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fried', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BaconNChz', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Joe Bidens Teeth', true);
+INSERT INTO burgers (burger_name) VALUES ('Owens Own');
